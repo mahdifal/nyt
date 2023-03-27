4fbf313abe7687353297bdf08ea4400fdb5e3943
Mahdi Falamarzi <mahdifal@users.noreply.github.com>
2023-03-27 18:30:49 +0330

    update

--- src/apis/nyt.js
+++ src/apis/nyt.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `https://api.nytimes.com/svc/search/v2/articlesearch.json`,
+  baseURL: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=Hh4BBgBu5AyzPkLCAs7iccpOT2GM3E14`,
 });
